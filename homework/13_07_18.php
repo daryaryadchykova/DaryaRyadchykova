eb9889cf02796f2c9b3c96ec52574c0dcab5d34e
daryaryadchykova <ryadchykovadarya@gmail.com>
2018-07-13 12:03:13 +0300

    HomeWork_13_07_18

--- homework/13_07_18.php
+++ homework/13_07_18.php
@@ -4,27 +4,101 @@
 <head>
     <meta charset="UTF-8">
     <title>
-        <?php echo $var; ?>
     </title>
 </head>
 <body>
+
+
+
 <?php
-echo ('Задача 1');
+echo ('<p>'.'Задача 1'.'</p>'). '</br>';
 
+$products=array(
+        ['name'=>'Телевизор','price'=>'400','quantity'=>1],
+        ['name'=>'Телефон','price'=>'300','quantity'=>3],
+        ['name'=>'Ноутбук','price'=>'150','quantity'=>2]);
 
-$i=1;
-echo '<table>';
-while($i<=10) {
-    echo '<tr>';
-    $n = 1;
-    while ($n <= 3) {
-        echo '<td>aaaa</td>';
-        $i++;
-    }
-    echo '</tr>';
-    $i++;
+function basket($products){
+    $totalPrice = 0;
+    $totalQuantity = 0;
+        foreach ($products as $product) {
+            $totalPrice += $product['price']*$product['quantity'];
+            $totalQuantity += $product['quantity'];
+        }
+    $basket = array($totalQuantity,$totalPrice);
+    return  $basket;
 }
-echo '</table>'
+
+$basket = basket($products);
+echo ('Количество покупок: ').$basket[0].'</br>';
+echo 'Сумма Вашего заказа: '.$basket[1].'</br>';
+
+
+
+echo ('<p>'.'Задача 2'.'</p>'). '</br>';
+echo ('10x^2 + 21x - 10 = 0;'). '</br>';
+
+$a = 10;
+$b = 21;
+$c = -10;
+
+function equation ($a, $b, $c){
+    $d = ($b ** 2) - (4 * $a * $c);
+
+    if ($d > 0){
+        $x1 = ((-1 * $b) - sqrt($d)) / (2 * $a);
+        $x2 = ((-1 * $b) + sqrt($d)) / (2 * $a);
+        return [$x1, $x2];
+    } elseif ($d == 0) {
+        $x3= (-1 * $b) / (2*$a);
+        return [$x3];
+    } else {
+        return false;
+    }
+}
+
+$answer= equation ($a,$b,$c);
+//echo count($answer);
+$count=count($answer);
+if($count=2){
+    echo ('x1=').$answer[0] . '</br>';
+    echo ('x2=').$answer[1] . '</br>';
+}
+elseif($count=1){
+    echo ('Уравнение имеет 1 действительный корень x =').$answer[0] . '</br>';
+}
+else{
+    echo('Уравнение не имеет корней');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//$i2=1;
+//echo '<table>';
+//while($i2<=3) {
+    //echo '<tr>';
+    //$n = 1;
+    //while ($n <= 3) {
+      //  echo '<td>aaaa</td>';
+        //$n++;
+    //}
+    //echo '</tr>';
+    //$i2++;
+//}
+//echo '</table>'
  ?>
 
 </body>
